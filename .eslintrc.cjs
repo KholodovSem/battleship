ce49bf5bc8ddd9e2e15e8becac1cb9b2cce3f248
Kholodov Sem <sem.kholodov@madappgang.com>
2023-06-09 17:37:50 +0300

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    "prettier"
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', project: './tsconfig.json' },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'airbnb-typescript',
+      ],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  ],
+}
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './App.css';
+import 'normalize.css';
+
+function App() {
+  return <div>Sea Wars</div>;
+}
+
+export default App;
