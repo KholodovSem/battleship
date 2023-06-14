abd9c2c38f3205ba53261f1adacd6d9bec19d1d2
Kholodov Sem <sem.kholodov@madappgang.com>
2023-06-14 12:46:24 +0300

    Improved ship interface and apapt fn to them

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,8 +4,6 @@ import { BorderSide } from '@/components/BorderSide';
 
 const BOARD_HEIGHT = 10;
 const BOARD_WIDTH = 10;
-const BOARD_SIZE = BOARD_HEIGHT * BOARD_WIDTH;
-const CELL_FOR_BOARD = [...Array(BOARD_SIZE).keys()].map(item => item);
 const CELL_TYPE_IN_HTML = 'board-cell';
 
 const NUMERIC_SIDE = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -19,17 +17,31 @@ Big ship - 2;
 Medium ship - 3;
 Small ship - 4;
 
+TOTAL = 10;
 */
 
+//Huge ship takes 4 cells
+//Big ship takes 3 cells
+//Medium ship takes 2 cells
+//Small ship takes 1 cells
 type ShipType = 'Huge ship' | 'Big ship' | 'Medium ship' | 'Small ship';
 
+//Possible orientation is "Horizontal" or "Vertical"
 type ShipOrientation = 'horizontal' | 'vertical';
+
+//Actually it's cell index
+type RowIndex = number;
+type ColumnIndex = number;
+
+//RowIndex - each vertical cell
+//ColumnIndex - each horizontal cell
+type ShipCoordinates = [RowIndex, ColumnIndex];
 
 interface Ship {
   type: ShipType;
-  orientation: ShipOrientation; //possible orientation is "Horizontal" or "Vertical"
-  coordinates: number[]; //indexes of the cells
-  damaged: number[]; //similarly to the coordinates
+  orientation: ShipOrientation;
+  coordinates: ShipCoordinates[];
+  damaged: ShipCoordinates[];
 }
 
 //TODO: Handle dublicates in "coordinates", "damaged", "missedShoots" when handlingBoardClick.
@@ -38,28 +50,49 @@ const initialState: Ship[] = [
   {
     type: 'Huge ship',
     orientation: 'horizontal',
-    coordinates: [1, 2, 3, 4],
+    coordinates: [
+      [0, 1],
+      [0, 2],
+      [0, 3],
+      [0, 4],
+    ],
     damaged: [],
   },
 ];
 
 export const Board = () => {
   const [ships, setShips] = useState<Ship[]>(initialState);
-  const [missedShoots, setMissedShoots] = useState<number[]>([]); //stores indexes of missed cells
+  const [missedShoots, setMissedShoots] = useState<ShipCoordinates[]>([]);
 
   const boardWrapperStyle =
     'relative w-full h-screen flex items-center justify-center';
   const boardStyle =
     'relative w-full h-full max-w-[600px] max-h-[600px] grid grid-cols-[repeat(10,1fr)] gap-[1px]';
 
-  const findShipByOccupiedCell = (cellIndex: number) =>
-    ships.find(ship => ship.coordinates.includes(cellIndex));
+  //Function takes two corteges with coordinates and makes array decomposition
+  //then it compares rowIndex & columnIndex from each tuple
+  const isEqualCoordinates = (
+    coordinates1: ShipCoordinates,
+    coordinates2: ShipCoordinates
+  ) => {
+    const [rowIndex1, columnIndex1] = coordinates1;
+    const [rowIndex2, columnIndex2] = coordinates2;
+    return rowIndex1 === rowIndex2 && columnIndex1 === columnIndex2;
+  };
 
-  const findShipByHittedCell = (cellIndex: number) =>
-    ships.find(ship => ship.damaged.includes(cellIndex));
+  //Array.some() - Checks if at least one element of the array satisfies the condition
+  const findShipByOccupiedCell = (coordinates: ShipCoordinates) =>
+    ships.find(ship =>
+      ship.coordinates.some(coord => isEqualCoordinates(coord, coordinates))
+    );
 
-  const isMissedCell = (cellIndex: number) => {
-    return missedShoots.includes(cellIndex);
+  const findShipByHittedCell = (coordinates: ShipCoordinates) =>
+    ships.find(ship =>
+      ship.damaged.some(coord => isEqualCoordinates(coord, coordinates))
+    );
+
+  const isMissedCell = (coordinates: ShipCoordinates) => {
+    return missedShoots.some(coord => isEqualCoordinates(coord, coordinates));
   };
 
   //We using event delegation for click handling
@@ -71,11 +104,16 @@ export const Board = () => {
 
     if (!cell) return;
 
-    const cellIndex = parseInt(cell.getAttribute('data-index')!); //getAttribute return string, we need parse it to number
-    const ship = findShipByOccupiedCell(cellIndex);
+    //getAttribute return string, we need parse it to number
+    const rowIndex = parseInt(cell.getAttribute('data-row-index')!);
+    const columnIndex = parseInt(cell.getAttribute('data-column-index')!);
+    const coordinates: ShipCoordinates = [rowIndex, columnIndex];
+
+    const ship = findShipByOccupiedCell(coordinates);
 
     if (ship) {
-      const damaged = [...ship.damaged, cellIndex];
+      console.log('A');
+      const damaged = [...ship.damaged, coordinates];
       const updatedShip = { ...ship, damaged };
       setShips(currentShips =>
         currentShips.map(currentShip =>
@@ -85,7 +123,10 @@ export const Board = () => {
       return;
     }
 
-    setMissedShoots(currentMissedShoots => [...currentMissedShoots, cellIndex]);
+    setMissedShoots(currentMissedShoots => [
+      ...currentMissedShoots,
+      coordinates,
+    ]);
   };
 
   return (
@@ -93,24 +134,29 @@ export const Board = () => {
       <div className={boardStyle} onClick={handleBoardClick}>
         <BorderSide items={NUMERIC_SIDE} position="top" />
         <BorderSide items={ALPHABETIC_SIDE} position="left" />
-        {CELL_FOR_BOARD.map(cellIndex => {
-          let type: null | CellType = null;
+        {Array.from({ length: BOARD_HEIGHT }, (_, rowIndex) =>
+          Array.from({ length: BOARD_WIDTH }, (_, columnIndex) => {
+            const coordinates: ShipCoordinates = [rowIndex, columnIndex];
 
-          if (findShipByOccupiedCell(cellIndex)) type = CellType.OCCUPIED;
-          if (findShipByHittedCell(cellIndex)) type = CellType.HITTED;
-          if (isMissedCell(cellIndex)) type = CellType.MISS;
+            let type: null | CellType = null;
 
-          //In another case type = null, that we handling with ?? operator that return right operator if left one is null or undefined
+            if (findShipByOccupiedCell(coordinates)) type = CellType.OCCUPIED;
+            if (findShipByHittedCell(coordinates)) type = CellType.HITTED;
+            if (isMissedCell(coordinates)) type = CellType.MISS;
 
-          return (
-            <Cell
-              key={cellIndex}
-              type={type ?? CellType.EMPTY}
-              data-type={CELL_TYPE_IN_HTML}
-              data-index={cellIndex}
-            />
-          );
-        })}
+            //In another case type = null, that we handling with ?? operator that return right operator if left one is null or undefined
+
+            return (
+              <Cell
+                key={`${rowIndex}-${columnIndex}`}
+                type={type ?? CellType.EMPTY}
+                data-type={CELL_TYPE_IN_HTML}
+                data-row-index={rowIndex}
+                data-column-index={columnIndex}
+              />
+            );
+          })
+        )}
       </div>
     </div>
   );
